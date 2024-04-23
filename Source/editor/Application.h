dec447a19efc01b5c16ea02fe1be8f15e2cba572
Diego E. Moreno <diegomoreno.pro45@gmail.com>
2024-04-23 10:39:57 +0200

    Initial commit

---
+++ Source/editor/Application.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#ifdef WL_HEADLESS
+	#include "Walnut/ApplicationHeadless.h"
+#else
+	#include "../../Platform/GUI/editor/ApplicationGUI.h"
+#endif
---
+++ uikit.h
@@ -0,0 +1,12 @@
+#ifndef UIKIT_H
+#define UIKIT_H
+
+#include "./Platform/GUI/editor/UI/Spinner.h"
+#include "./Source/editor/Layer.h"
+#include "./Source/editor/Random.h"
+#include "./Platform/GUI/editor/ImGui/ImGuiTheme.h"
+#include "./Platform/GUI/editor/Image.h"
+#include "./Platform/GUI/editor/ApplicationGUI.h"
+#include "./Platform/GUI/editor/UI/UI.h"
+
+#endif // UIKIT_H
