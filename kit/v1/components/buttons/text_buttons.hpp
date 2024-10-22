2ee54283a65490bccf12d255902286fb9de3c140
diegomrno <diegomoreno.pro45@gmail.com>
2024-10-22 16:51:27 +0200

    feat: New component approach

--- kit/v1/components/buttons/text_buttons.hpp
+++ kit/v1/components/buttons/text_buttons.hpp
@@ -5,30 +5,35 @@
 
 namespace Cherry
 {
+    struct TextButtonUnderlineOptions
+    {
+        std::string hex_text_idle = "#999999ff";
+        std::string hex_text_hovered = "#BBBBBBff";
+        std::string hex_text_clicked = "#FFFFFFff";
+        std::string hex_underline_idle = "#494949ff";
+        std::string hex_underline_hovered = "#BBBBBBff";
+        std::string hex_underline_clicked = "#FFFFFFff";
+        ImVec2 size = ImVec2(0, 0);
+        float y_margin = 2.0f;
+    };
+
     inline bool TextButtonUnderline(
         const std::string &label,
         bool enabled = true,
-        const std::string &hex_text_idle = "#999999ff",
-        const std::string &hex_text_hovered = "#BBBBBBff",
-        const std::string &hex_text_clicked = "#FFFFFFff",
-        const std::string &hex_underline_idle = "#494949ff",
-        const std::string &hex_underline_hovered = "#BBBBBBff",
-        const std::string &hex_underline_clicked = "#FFFFFFff",
-        const ImVec2 &size = ImVec2(0, 0),
-        float y_margin = 2.0f)
+        const TextButtonUnderlineOptions &options = TextButtonUnderlineOptions())
     {
         bool pressed = false;
         int i = 0;
 
-        ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, y_margin));
-        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, y_margin));
+        ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, options.y_margin));
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, options.y_margin));
 
-        ImVec4 text_idle = HexToRGBA(hex_text_idle.c_str());
-        ImVec4 text_hovered = HexToRGBA(hex_text_hovered.c_str());
-        ImVec4 text_clicked = HexToRGBA(hex_text_clicked.c_str());
-        ImVec4 underline_idle = HexToRGBA(hex_underline_idle.c_str());
-        ImVec4 underline_hovered = HexToRGBA(hex_underline_hovered.c_str());
-        ImVec4 underline_clicked = HexToRGBA(hex_underline_clicked.c_str());
+        ImVec4 text_idle = HexToRGBA(options.hex_text_idle.c_str());
+        ImVec4 text_hovered = HexToRGBA(options.hex_text_hovered.c_str());
+        ImVec4 text_clicked = HexToRGBA(options.hex_text_clicked.c_str());
+        ImVec4 underline_idle = HexToRGBA(options.hex_underline_idle.c_str());
+        ImVec4 underline_hovered = HexToRGBA(options.hex_underline_hovered.c_str());
+        ImVec4 underline_clicked = HexToRGBA(options.hex_underline_clicked.c_str());
 
         ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
         ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0, 0, 0, 0));
@@ -36,7 +41,7 @@ namespace Cherry
         ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0, 0, 0, 0));
         ImGui::PushStyleColor(ImGuiCol_Text, text_idle);
 
-        if (ImGui::Button(label.c_str(), size))
+        if (ImGui::Button(label.c_str(), options.size))
         {
             pressed = true;
         }
