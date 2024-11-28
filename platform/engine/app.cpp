e19a4286725fa78868b8e25cbb3e05342f87495f
diegomrno <diegomoreno.pro45@gmail.com>
2024-11-28 20:57:47 +0100

    fix: g_ApplicationRunning undefined with win DLL

--- platform/engine/app.cpp
+++ platform/engine/app.cpp
@@ -27,7 +27,7 @@
 #include "imgui/Hack-Regular.embed"
 #include "imgui/Inconsolatas.embed"
 
-extern bool g_ApplicationRunning;
+extern bool g_ApplicationRunning = false;
 
 // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
 // To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
