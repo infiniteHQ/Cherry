dc14825d998352dca019e27b60efdfcaef60e9cc
diegomrno <diegomoreno.pro45@gmail.com>
2025-01-21 10:52:15 +0100

    feat: Optional net code compilation

--- options.hpp
+++ options.hpp
@@ -1,2 +1,7 @@
-#define CHERRY_NET // Enable all the net code (needs curl) (feats: Http fetchs)
+
+// AUTO ENABLED BY CMAKE COMPILATION
+//#define CHERRY_CEF // Enable all the net code (needs curl) (feats: Http fetchs)
+//#define CHERRY_NET // Enable all the net code (needs curl) (feats: Http fetchs)
+
+// MANUAL
 #define CHERRY_V1 // Enable V1 prebuild components
