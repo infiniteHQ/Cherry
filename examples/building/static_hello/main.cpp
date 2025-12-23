6eff6fe4efa03b35b9653faf2e07bc4920aff33e
diegomrno <diegomoreno.pro45@gmail.com>
2025-12-23 17:50:18 +0100

    feat: New build examples

---
+++ examples/building/static_hello/main.cpp
@@ -0,0 +1,12 @@
+#include "../../cherry.hpp"
+
+CherryApplication *Cherry::CreateApplication(int argc, char **argv) {
+  Cherry::ApplicationSpecification config;
+  config.SetMainRenderCallback([]() { CherryKit::TitleOne("Hello World"); });
+  return new CherryApplication(config);
+}
+
+int main(int argc, char *argv[]) {
+  CherryRun(argc, argv);
+  return 0;
+}
