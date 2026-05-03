f754a628ecd413051e4f040ace9ffb15b98cdb1a
diegomrno <d@infinite.si>
2026-05-03 22:27:53 +0200

    feat: Add shaders API base files

---
+++ main/engine/shaders/shaders.hpp
@@ -0,0 +1,10 @@
+//
+//  shaders.hpp
+//  Headers for Cherry shaders API
+//
+//  Copyright (c) 2024-2026 Diego Moreno
+//  Copyright (c) 2026 Infinite
+//
+//	This work is licensed under the terms of the MIT license.
+//	For a copy, see <https://opensource.org/licenses/MIT>.
+//
