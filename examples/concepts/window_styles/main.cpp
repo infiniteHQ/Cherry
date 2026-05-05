dbb4771090378dc3b74fedb2184ec1889ff3332e
diegomrno <d@infinite.si>
2026-05-05 20:58:02 +0200

    feat: Populate window style example

--- examples/concepts/window_styles/main.cpp
+++ examples/concepts/window_styles/main.cpp
@@ -1,23 +1,56 @@
 #include "../../../cherry.hpp"
 
-void AppWindowRender() {
-  std::string WindowName = CherryWindow.GetName();
-  std::string AppWindowName = CherryAppWindow.GetName();
+void RenderNoCustomTitleBar() {
+  CherryKit::TextSimple("Hello from no custom titlebar window !");
+}
+void RenderCustomTitleBar() {
+  CherryKit::TextSimple("Hello from custom titlebar window !");
+}
 
-  CherryKit::TextSimple("I'm \"" + AppWindowName + "\" and i'm on the window \"" + WindowName + "\"");
+void AppWindowRender() {
+  if (CherryKit::ButtonText("Spawn window with no custom titlebar").GetDataAs<bool>("isClicked")) {
+    auto ap = CherryKit::WindowSimple("No titlebar", RenderNoCustomTitleBar);
+    Cherry::ApplicationSpecification spec;
+    spec.SetName("No titlebar");
+    spec.SetRenderMode(Cherry::WindowRenderingMethod::SimpleWindow);
+    spec.SetUniqueAppWindowName("No titlebar");
+    spec.SetMinimumHeight(200);
+    spec.SetMinimumWidth(100);
+    spec.SetDefaultHeight(450);
+    spec.SetDefaultWidth(450);
+    spec.SetCustomTitlebar(false);
+    spec.DisableWindowManagerTitleBar = false;
+    ap->AttachOnNewWindow(spec);
+    Cherry::AddAppWindow(ap);
+  }
+
+  if (CherryKit::ButtonText("Spawn window with custom titlebar").GetDataAs<bool>("isClicked")) {
+    auto ap = CherryKit::WindowSimple("Custom titlebar", RenderCustomTitleBar);
+    Cherry::ApplicationSpecification spec;
+    spec.SetName("Custom titlebar");
+    spec.SetRenderMode(Cherry::WindowRenderingMethod::SimpleWindow);
+    spec.SetUniqueAppWindowName("Custom titlebar");
+    spec.SetMinimumHeight(200);
+    spec.SetMinimumWidth(100);
+    spec.SetDefaultHeight(450);
+    spec.SetDefaultWidth(450);
+    spec.SetCustomTitlebar(true);
+    spec.DisableWindowManagerTitleBar = true;
+    ap->AttachOnNewWindow(spec);
+    Cherry::AddAppWindow(ap);
+  }
 }
 
 CherryApplication CherryMain(int argc, char **argv) {
   // Configuration of the application
   Cherry::ApplicationSpecification config;
-  config.SetRenderMode(Cherry::WindowRenderingMethod::DockingWindows);
+  config.SetUniqueAppWindowName("Main");
+  config.SetRenderMode(Cherry::WindowRenderingMethod::SimpleWindow);
 
   auto app = new CherryApplication(config);
 
   // When created, let's add some components to the runtime
-  Cherry::AddAppWindow(CherryKit::WindowSimple("App Window 1", AppWindowRender));
-  Cherry::AddAppWindow(CherryKit::WindowSimple("App Window 2", AppWindowRender));
-  Cherry::AddAppWindow(CherryKit::WindowSimple("App Window 3", AppWindowRender));
+  Cherry::AddAppWindow(CherryKit::WindowSimple("Main", AppWindowRender));
   return app;
 }
 
